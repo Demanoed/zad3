951f7605d08dbca3bdaccc5feab85b4f476fae71
Дмитрий Трефилов <trefilowdem@mail.ru>
2019-06-26 00:18:33 +0500

    Add comments

--- PopalNePopal/PopalNePopal/Program.cs
+++ PopalNePopal/PopalNePopal/Program.cs
@@ -9,7 +9,10 @@ namespace PopalNePopal
 {
     class Program
     {
+        #region Координаты точки
         private static int x, y;
+        #endregion
+        #region Ввод координат
         private static void ZaprosXY()
         {
             ColorMess.Yellow("\n Введите координату точки X: ");
@@ -17,6 +20,8 @@ namespace PopalNePopal
             ColorMess.Yellow("\n Введите координату точки Y: ");
             y = Input.Check(int.MinValue, int.MaxValue);
         }
+        #endregion
+        #region Проверка на принадлежность плоскости
         private static bool ChekPopadanie()
         {
             bool ploskost1 = (x <= 0 && y <= 0 && y >= -1 && y <= (3 * x + 2));
@@ -26,6 +31,8 @@ namespace PopalNePopal
             if (ploskost1 || ploskost2 || ploskost3 || ploskost4) return true;
             else return false;
         }
+        #endregion
+        #region Вывод результата
         private static void PrintResult(bool ok)
         {
             if (ok)
@@ -33,11 +40,14 @@ namespace PopalNePopal
             else
                 ColorMess.Red("\n Точка не попала в область");
         }
+        #endregion
+        #region Стартовая позиция программы, служит для ввода координат точки и печати результата в соответствии с заданием
         static void Main()
         {
             ZaprosXY();
             PrintResult(ChekPopadanie());
             Message.GoToBack();
         }
+        #endregion
     }
 }
