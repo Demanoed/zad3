f558597eaee8adcdef3e825306df1d49d46ded59
Дмитрий Трефилов <trefilowdem@mail.ru>
2019-06-26 01:37:43 +0500

    Removing unused directives

--- PopalNePopal/PopalNePopal/Program.cs
+++ PopalNePopal/PopalNePopal/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DemanT_Library;
+﻿using DemanT_Library;
 
 namespace PopalNePopal
 {
